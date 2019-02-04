2313f31be696a69630f58b47e1cd64d341903267
Evgeniy Nochevnov <xpath@e1.ru>
2019-02-04 21:20:17 +0300

    more generated data!

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,159 +6,67 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-users = [
-  # 1
-  {
-    attributes: {email: 'sls8sdf@ljl.aa'},
-    user_orders: [
-      attributes: {product: 'pencil 216', price: 0.4}
-    ]
-  },
-  # 2
-  {
-    attributes: {email: 'ljaljlk22l@qnewl.qq'},
-    user_orders: [
-      {
-        attributes: {product: 'star 77', price: 1.2},
-        bonus_log: {
-          attributes: {bonuses: 0.12},
-          referrer_email: 'sls8sdf@ljl.aa' # 1
-        }
-      },
-      {
-        attributes: {product: 'pioneer 3', price: 4}
-      }
-    ]
-  },
-  # 3
-  {
-    attributes: {email: 'oasd8a8sd@ljlqe.df'},
-    user_orders: [
-      {
-        attributes: {product: 'ebook 12.7', price: 11.8},
-        bonus_log: {
-          attributes: {bonuses: 1.18},
-          referrer_email: 'sls8sdf@ljl.aa' # 1
-        }
-      }
-    ]
-  },
-  # 4
-  {
-    attributes: {email: 'asd8s9@l2l.wp'},
-    user_orders: [
-      {
-        attributes: {product: 'umbrella 1.1', price: 4.3},
-        bonus_log: {
-          attributes: {bonuses: 0.43},
-          referrer_email: 'ljaljlk22l@qnewl.qq' # 2
-        }
-      }
-    ]
-  },
-  # 5
-  {
-    attributes: {email: 'sds9@lbt.eu'}
-  },
-  # 6
-  {
-    attributes: {email: 'vsvs9vs@bt.wa'},
-    user_orders: [
-      {
-        attributes: {product: 'lamp 3r', price: 2.8}
-      }
-    ]
-  },
-  # 7
-  {
-    attributes: {email: 'lnc3@4w.wo'},
-    user_orders: [
-      {
-        attributes: {product: 'graal 2', price: 82.1}
-      }
-    ]
-  },
-  # 8
-  {
-    attributes: {email: 'cwljkl@llkt.ew'},
-    user_orders: [
-      {
-        attributes: {product: 'phone 243', price: 7}
-      }
-    ]
-  },
-  # 9
-  {
-    attributes: {email: 'ljadls0@ldas.vi'},
-    user_orders: [
-      {
-        attributes: {product: 'court @1', price: 18}
-      }
-    ]
-  },
-  # 10
-  {
-    attributes: {email: 'moca8@mo.vs'},
-    user_orders: [
-      {
-        attributes: {product: 'fax #a', price: 1.7}
-      }
-    ]
-  },
-  # 11
-  {
-    attributes: {email: 'mi7v@kwv.bu'},
-    user_orders: [
-      {
-        attributes: {product: 'device 2', price: 4}
-      }
-    ]
-  },
-  # 12
-  {
-    attributes: {email: 'njuy7@i8.cr'},
-    user_orders: [
-      {
-        attributes: {product: 'gem 7.1', price: 7.1}
-      }
-    ]
-  },
-  # 13
-  {
-    attributes: {email: 'l7w@jl.bw'},
-    user_orders: [
-      {
-        attributes: {product: 'mouse trap', price: 2.4}
-      }
-    ]
-  },
-  # 14
-  {
-    attributes: {email: 'muc3c9@jlqc.ec'},
-    user_orders: [
-      {
-        attributes: {product: 'case 0.8', price: 1.1}
-      }
-    ]
-  }
-]
-
-User.transaction do
-  users.each do |user|
-    u = User.create!(user[:attributes])
+module DataGenerator
+  ALPHA = ('a'..'z').to_a
+  ALPHA_NUM = ALPHA + (0..9).to_a
+  
+  class << self
+    def email
+      name_length   = rand(18) + 2
+      domain_length = rand(12) + 2
+      zone_length   = rand(4)  + 2
+      
+      name   = name_length.times.map   { |t| ALPHA_NUM.sample }.join
+      domain = domain_length.times.map { |t| ALPHA_NUM.sample }.join
+      zone   = zone_length.times.map   { |t| ALPHA.sample }.join
+      
+      "#{name}@#{domain}.#{zone}"
+    end
     
-    if user[:user_orders]
-      user[:user_orders].each do |order|
-        o = u.user_orders.create!(order[:attributes])
-        
-        if order[:bonus_log]
-          ref = User.find_by(email: order[:bonus_log][:referrer_email])
-          account = ref.bonus_account || ref.create_bonus_account!
+    def product
+      length = rand(18) + 2
+      length.times.map { |t| ALPHA_NUM.sample }.join
+    end
+    
+    def price
+      (rand * 100).ceil(2)
+    end
+    
+    def orders_count
+      rand(3)
+    end
+    
+    def referrer
+      d = User.select('min(id) as min_id, max(id) as max_id')[0]
+      boundary = rand(d.max_id - d.min_id) + d.min_id
+      User.find_by('id >= ?', boundary)
+    end
+    
+    def coin?
+      rand(2) == 0
+    end
+  end
+end
+
+1_000.times do
+  User.transaction do
+    user = User.create!(email: DataGenerator.email)
+    
+    DataGenerator.orders_count.times do |t|
+      order = user.user_orders.create!(
+        product: DataGenerator.product,
+        price: DataGenerator.product
+      )
+      
+      if t == 0 && DataGenerator.coin?
+        referrer = DataGenerator.referrer
+        if referrer
+          account = referrer.bonus_account || referrer.create_bonus_account!
           
-          log = o.create_bonus_log!(
+          log = order.create_bonus_log!(
             bonus_account_id: account.id,
-            user_order_id: o.id,
-            bonuses: o.bonus_amount
+            user_order_id: order.id,
+            bonuses: order.bonus_amount
           )
           
           account.update!(bonuses: account.bonuses + log.bonuses)
